1b33e35a83bb8d4a201998e77d394025d3dcdd0d
Elliott <starlliott@gmail.com>
2026-01-07 21:55:39 -0500

    Create GyroNavX implementation to fix Gyro interface instantiation error
    Gyro is an interface and cannot be instantiated directly with 'new Gyro()'. Created GyroNavX class that implements the Gyro interface for NavX hardware (assuming we're using the same gyro as last year). Ian did not build his code and apparently does not know you cannot instantiate an interface...

---
+++ src/main/java/frc/robot/subsystems/Drivetrain/GyroNavX.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot.subsystems.Drivetrain;
+
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class GyroNavX implements Gyro {
+  private final AHRS gyro = new AHRS(NavXComType.kUSB1);
+
+  public GyroNavX() {
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Rotation2d getRotation2d() {
+    return gyro.getRotation2d();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public double getRate() {
+    return gyro.getRate();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * Note that it is not possible to actually calibrate the NavX gyro as it is
+   * calibrated on power up. This method will only zero the gyro.
+   */
+  @Override
+  public void calibrate() {
+    gyro.zeroYaw();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setAngle(Rotation2d angle) {
+    gyro.zeroYaw();
+    gyro.setAngleAdjustment(angle.getDegrees());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isConnected() {
+    return gyro.isConnected();
+  }
+}
