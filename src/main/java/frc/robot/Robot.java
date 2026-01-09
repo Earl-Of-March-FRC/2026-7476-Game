a25f14a767a83a1de63c47951124d8edb7a4cc94
wyu4 <wilsonyu657@gmail.com>
2026-01-09 16:50:42 -0500

    Add WPILOGWriter + URCL to AdvantageKit
    WPILOGWriter is used to automatically create and write .wpilog files on the RIO or on a FAT32 USB chip. URCL is an extension that automatically includes CAN frames from REV devices in the logs.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,8 @@ package frc.robot;
 
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+import org.littletonrobotics.urcl.URCL;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -33,7 +33,13 @@ public class Robot extends LoggedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
+
     // Start logger
+    Logger.recordMetadata("ProjectName", "2026-7576-game");
+    if (isReal()) {
+      Logger.addDataReceiver(new WPILOGWriter());
+      Logger.registerURCL(URCL.startExternal());
+    }
     Logger.start();
 
     m_robotContainer = new RobotContainer();
