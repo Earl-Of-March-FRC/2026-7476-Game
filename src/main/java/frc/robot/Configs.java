29c6d6d2d03a6b0c1e0fa544761d536da9d8e15f
wyu4 <wilsonyu657@gmail.com>
2026-01-16 22:17:01 -0500

    Clean imports in Configs.java

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
